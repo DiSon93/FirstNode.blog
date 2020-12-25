1d6a457e562cd32746ea36262278ab7b42a03f4a
DiSon93 <sonnguyendi93@gmail.com>
2020-12-25 23:33:49 +0700

    install prettier

--- src/index.js
+++ src/index.js
@@ -1,36 +1,33 @@
 const path = require('path');
 
-const express = require('express')
+const express = require('express');
 //sử dụng morgan để theo dõi xong request đã gửi tới serve hay chưa
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-const handlebars  = require('express-handlebars');
+const handlebars = require('express-handlebars');
 
-const app = express()
+const app = express();
 const port = 3000;
 
-const route = require ('./routes');
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//XMLHttpResquest, fetch, axios 
+//XMLHttpResquest, fetch, axios
 
 app.use(morgan('combined'));
 
-app.engine('hbs', handlebars({extname: '.hbs'}));
+app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH',path.join(__dirname, 'resources/views'));
 
-
 //Route init
 route(app);
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
